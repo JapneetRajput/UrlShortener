d6e91c74abddc59ef9ffa4b024cb7c9747790ab9
Japneet Rajput <japneetrajput2804@gmail.com>
2022-05-13 21:24:42 +0530

    Profile Implemented

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template,request,session,redirect,url_for,g
 from pyshorteners import *
-# from flaskext.mysqldb import MySQL 
 import mysql.connector
-# from flask.ext.mysqldb import MySQL 
 
 shortener = Shortener()
 
 app = Flask(__name__)
-# app.config['MYSQL_HOST'] = 'japneet-python-db.mysql.database.azure.com'
-# app.config['MYSQL_USER'] = '1FcvJ1A4lW'
-# app.config['MYSQL_PASSWORD'] = 'Commando@007'
-# app.config['MYSQL_DB'] = '1FcvJ1A4lW'
-# mysql = MySQL(app)
 
 app.secret_key="japneet"
 
@@ -26,9 +19,9 @@ def beforeLogin():
 @app.route('/login',methods=['POST','GET'])
 def login():
     mydb=mysql.connector.connect(
-        host="japneet-python-db.mysql.database.azure.com",
-        user="japneet@japneet-python-db",
-        password="Commando@",
+        host="localhost",
+        user="root",
+        password="",
         database="username"
     )
     mycursor = mydb.cursor()
@@ -39,18 +32,24 @@ def login():
         email = signup['email']
         passw = signup['pass']
         rpassw = signup['repass']
+        # mycursor.execute("select * from users where Username='"+username+"'")
+        # count=mycursor.rowcount
+        # if count ==1:
+        #     return "A user already exists with this username."
+        # else:
         mycursor.execute("insert into users values(%s,%s,%s,%s,%s)",(name,username,email,passw,rpassw))
         mydb.commit()
-        mycursor.close()
         return render_template('urlShortener.html')
+    else:
+        return render_template('urlShortener.html')
+    mycursor.close()
     
-
 @app.route('/urlShortener',methods=['POST','GET'])
 def urlShortener():
     mydb=mysql.connector.connect(
-        host="japneet-python-db.mysql.database.azure.com",
-        user="japneet@japneet-python-db",
-        password="Commando@",
+        host="localhost",
+        user="root",
+        password="",
         database="username"
     )
     mycursor = mydb.cursor()
@@ -65,36 +64,19 @@ def urlShortener():
         count=mycursor.rowcount
         if count ==1:
             return render_template('url.html')
-        elif count>1:
-            return "More than one user"
         else:
-            return "You are not a member please <a href='/register.html'>register</a>"
+            return "You are not a member please <a href='/beforeRegister'>register</a>"
     else:
         return render_template('url.html')
     mydb.commit()
     mycursor.close()
     
-@app.route('/history')
-def history():
-    mydb=mysql.connector.connect(
-        host="japneet-python-db.mysql.database.azure.com",
-        user="japneet@japneet-python-db",
-        password="Commando@",
-        database="username"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("select * from link")
-    r=mycursor.fetchall()
-    username = session['username']
-    len1 = len(r)
-    return render_template('history.html',r=r,len1=len1,username=username)
-
 @app.route('/url',methods=['POST','GET'])
 def url():
     mydb=mysql.connector.connect(
-        host="japneet-python-db.mysql.database.azure.com",
-        user="japneet@japneet-python-db",
-        password="Commando@",
+        host="localhost",
+        user="root",
+        password="",
         database="username"
     )
     mycursor = mydb.cursor()
@@ -112,11 +94,80 @@ def url():
         mycursor.close()
         return render_template('urlResult.html',result=result)
 
+@app.route('/history')
+def history():
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="username"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("select * from link")
+    r=mycursor.fetchall()
+    username = session['username']
+    len1 = len(r)
+    return render_template('history.html',r=r,len1=len1,username=username)
+
+@app.route('/beforeEditProfile')
+def beforeEditProfile():
+    return render_template('editProfile.html')
+    
+@app.route('/profile')
+def profile():
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="username"
+    )
+    mycursor = mydb.cursor()
+    username = session['username']
+    mycursor.execute("select * from users where Username='"+username+"'")
+    r=mycursor.fetchone()
+    mydb.commit()
+    mycursor.close()
+    return render_template('profile.html',r=r,username=username)
+
+@app.route('/editProfile',methods=['POST','GET'])
+def editProfile():
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="username"
+    )
+    mycursor = mydb.cursor()
+    usernamee = session['username']
+    mycursor.execute("select * from users where Username='"+usernamee+"'")
+    r=mycursor.fetchone()
+    if request.method=='POST':
+        signup=request.form
+        name = signup['name']
+        username = signup['username']
+        email = signup['email']
+        oldPassw = signup['oldPass']
+        newPassw = signup['newPass']
+        rpassw = signup['repass']
+        if oldPassw==newPassw:
+            if oldPassw == r[3]:
+                mycursor.execute("UPDATE `users` SET `Name`=%s,`Username`=%s,`Email`=%s,`Password`=%s,`ConfirmPassword`=%s WHERE Username=%s",(name,username,email,newPassw,rpassw,usernamee))
+                mydb.commit()
+                return render_template('profile.html',r=r,username=username)
+                session['username']=usernamee
+            else:
+                return "<h1>Old Password entered is incorrect.</h1>"
+        else:
+            return "<h1>Passwords don't match.</h1>"
+    return render_template('profile.html',r=r,username=username)
+    
+
 @app.route('/logout')
 def logout():
-    return "Logged out successfully"
+    return redirect(url_for('.index'))
+
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="https://japneeturlshortener.herokuapp.com/", port=8080)
+    serve(app, host="127.0.0.1", port=5000)
          
         
